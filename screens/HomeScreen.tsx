8020e12f6e37fb747e1f7c93c5f867f3aa1ae01d
SiddartthVS <siddartthvs4@gmail.com>
2026-09-15 22:51:54 +0530

    add custom font

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,10 +2,23 @@ import { Text, View } from "react-native";
 
 const HomeScreen = () => {
     return (
-        <View>
-            <Text>Welcome to the Home Screen!</Text>
+        <View style={Styles.container}>
+            <Text style={Styles.title}>Welcome to the Home Screen!</Text>
         </View>
     );
+};
+
+const Styles = {
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        color: '#eeeeee',
+        fontFamily: 'WorkSans-Black',
+    },
 };
 
 export default HomeScreen;
